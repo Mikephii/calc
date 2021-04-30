be6728a3cbc07ff16700eced0c34d5703d956f4a
Michael <mich.collins1@gmail.com>
2021-04-30 15:06:44 +1000

    basic funcitonality no styling

---
+++ script.js
@@ -0,0 +1,112 @@
+
+let resultValue = '';
+let numberValue = ''
+let n1 = ''
+let n2 = ''
+let op = ''
+
+let display = document.querySelector('#display');
+display.textContent = ''
+
+
+
+let buttonsn = document.querySelectorAll('.number');
+buttonsn.forEach(button => {button.addEventListener('click',updateNumber)
+    
+});
+
+let buttonso = document.querySelectorAll('.operator');
+buttonso.forEach(button => {button.addEventListener('click',operator)
+    
+});
+
+let equals= document.querySelector('#equals');
+equals.addEventListener('click',equalsResult)
+
+function equalsResult(){
+
+    if(n1 && numberValue && op){
+        n2= numberValue
+        display.textContent=operate(n1,n2,op);
+        n1=operate(n1,n2,op);
+        n2 = ''
+        op = ''
+        numberValue =''
+    }
+
+}
+
+function updateNumber(e){
+    if(!(!op&&n1)){
+        numberValue+=e.srcElement.id
+        display.textContent = display.textContent + e.srcElement.id
+    }
+}
+
+function operator(e){
+    if(!n1 && numberValue){
+        n1=numberValue
+        op=e.srcElement.id
+        display.textContent = display.textContent + e.srcElement.id
+        numberValue=''
+    }
+    if(n1 && op &&numberValue){
+        n2 = numberValue
+        
+        display.textContent=operate(n1,n2,op)+e.srcElement.id;
+
+        n1=operate(n1,n2,op);
+        
+        op=e.srcElement.id
+        n2=''
+        numberValue=''
+    }
+
+    if(n1 && !numberValue &&!op){
+        op=e.srcElement.id;
+        display.textContent = display.textContent + e.srcElement.id
+
+    }
+
+}
+
+
+function operate(n1,n2,argument){
+
+    switch(argument){
+        case '+':
+        return add(n1,n2);
+        break;
+        case '-':
+        return subtract(n1,n2);
+        break;
+        case '*':
+        return multiply(n1,n2)
+        break;
+        case '/':
+        return divide(n1,n2)
+        break;
+    }
+
+}
+
+
+
+
+function add (a,b) {
+	return Math.round((parseFloat(a,10) + parseFloat(b,10))*100)/100;
+}
+
+function subtract (a,b) {
+	return Math.round((parseFloat(a,10) - parseFloat(b,10))*100)/100;
+}
+
+
+function multiply (a,b) {
+	
+	return Math.round((a*b)*100)/100;
+}
+function divide (a,b) {
+	
+	return Math.round((a/b)*100)/100;
+}
